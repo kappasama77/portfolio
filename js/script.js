764809131597d028090b025dc76aa20258ea9979
kappasama77 <kappamama1201@gmail.com>
2026-04-15 11:36:06 +0900

    add js

--- js/script.js
+++ js/script.js
@@ -7,23 +7,6 @@ const swiper = new Swiper('.swiper', {
     },
 });
 // -------------------------------------------------------
-// start時にフルスクリーン
-window.addEventListener("load",function(){
-    const loading = document.getElementById("loading");
-    console.log(loading);
-
-    // フェードアウト
-    setTimeout(() => {
-        loading.classList.add("fade-out");
-
-        // アニメーション終わったら消す
-        setTimeout(() => {
-            loading.style.display = "none";
-        },1000);//終わる時間
-    }, 2000);//表示時間
-});
-
-// ------------------------------------------------------
 // クリックでスクロール
 document.getElementById("scrollTop").addEventListener("click",function(e) {
     e.preventDefault();
@@ -47,33 +30,47 @@ window.addEventListener("scroll",function() {
 });
 
 // -------------------------------------------------------------
-// 画像の切り替え
-// 目
-window.addEventListener("load",function() {
-    const  images1 = [
-        document.getElementById("eye-normal"),
-        document.getElementById("eye-wink")
-    ];
+//ローディングするときの動作
+window.addEventListener("load", function() {
     
-    let index1 = 0;
+    // -------------------
+    // 入ってすぐに浮かぶロゴの動き
+    const loading = document.getElementById("loading");
+    
+setTimeout(() => {
+    loading.classList.add("fade-out");
+    
+    setTimeout(() => {
+        loading.style.display = "none";
+    }, 1000);
+}, 2000);
+
+// -------------------
+// 画像の切り替え
+  // 目
+const images1 = [
+    document.getElementById("eye-normal"),
+    document.getElementById("eye-wink")
+];
+
+let index1 = 0;
 
 setInterval(() => {
     images1.forEach(img => {
-        if (img)
-            img.style.display = "none";
+    if (img) img.style.display = "none";
     });
 
     if (images1[index1]) {
-        images1[index1].style.display = "block";
+    images1[index1].style.display = "block";
     }
 
     index1 = (index1 + 1) % images1.length;
-}, 3000);
-});
+}, 1000);
 
-    // 文字
-window.addEventListener("load",function() {
-const  images2 = [
+
+  // -------------------
+  // 文字
+const images2 = [
     document.getElementById("Hi"),
     document.getElementById("hart"),
     document.getElementById("welcome")
@@ -83,20 +80,19 @@ let index2 = 0;
 
 setInterval(() => {
     images2.forEach(img => {
-        if (img) 
-            img.style.display = "none";
+    if (img) img.style.display = "none";
     });
 
     if (images2[index2]) {
-        images2[index2].style.display = "block";
+    images2[index2].style.display = "block";
     }
 
     index2 = (index2 + 1) % images2.length;
-}, 3000);
-});
-//背景
-window.addEventListener("load", function () {
+}, 2000);
 
+
+  // -------------------
+  // 背景
 const images3 = [
     document.getElementById("back-ck-blue"),
     document.getElementById("back-ck-green"),
@@ -107,28 +103,31 @@ let index3 = 0;
 
 setInterval(() => {
     images3.forEach(img => {
-if (img)img.style.display = "none";
+    if (img) img.style.display = "none";
     });
 
     if (images3[index3]) {
-images3[index3].style.display = "block";
+    images3[index3].style.display = "block";
     }
 
     index3 = (index3 + 1) % images3.length;
 }, 3000);
 
-});
-// ---------------------------------------------------------------
-// ハンバーガーメニュー
-window.addEventListener("load", function () {
-    var nav = document.getElementById('nav-wrapper');
-    var hamburger = document.getElementById('hamburger');
-    var blackBg = document.getElementById('js-black-bg');
 
+  // -------------------
+  // ハンバーガー
+var nav = document.getElementById('nav-wrapper');
+var hamburger = document.getElementById('hamburger');
+var blackBg = document.getElementById('js-black-bg');
+
+if (hamburger && blackBg) {
     hamburger.addEventListener('click', function () {
-        nav.classList.toggle('open');
+    nav.classList.toggle('open');
     });
+
     blackBg.addEventListener('click', function () {
-        nav.classList.remove('open');
+    nav.classList.remove('open');
     });
+}
+
 });
